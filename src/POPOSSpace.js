6fcdc99f637778d16fa464acfc049cc448b37219
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2026-09-24 11:35:27 -0700

    Poposspace reworked in tailwind

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,19 +1,29 @@
-import styles from './POPOSSpace.module.css'
+// import styles from './POPOSSpace.module.css'
 import { Link } from 'react-router-dom'
 
 function POPOSSpace(props) {
   const { name, image, address, hours, id } = props
   return (
-    <div className={styles.POPOSSpace} >
+    <div className='
+      w-full min-w-0 shadow-lg
+      md:overflow-hidden md:border md:border-brand md:rounded-lg
+    '>
       <Link to={`/details/${id}`}>
-        <img className={styles.POPOSSpaceImg}
+        <img
+          className='w-full h-auto 
+            object-cover object-center 
+            aspect-video lg:aspect-auto'
           src={`${process.env.PUBLIC_URL}/images/${image}`}
           alt={`${name}`} />
-        <div className={styles.POPOSSpaceText}>
-          <h1>{name}</h1>
-          <div className={styles.POPOSSpaceTextP}>
-            <p className={styles.POPOSSpaceTextPHrs}><em>{address}</em></p>
-            <p>{hours}</p>
+        <div className='m-0 py-2 px-4 
+          flex flex-1 justify-between gap-1
+          lg:flex-col lg:gap-2 lg:pb-4
+        '>
+          <h1 className='text-start m-0 text-2xl 
+            lg:text-center'>{name}</h1>
+          <div className='flex flex-col'>
+            <p className='m-1 lg:m-0 text-brand'><em>{address}</em></p>
+            <p className='m-1 lg:m-0'>{hours}</p>
           </div>
         </div>
       </Link>
