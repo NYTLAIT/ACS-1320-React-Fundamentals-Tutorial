df6cc95080601f8e9fdcf03de6a11e30adf657ac
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2026-09-24 17:49:22 -0700

    Title reworked - All reworked into tailwind

--- src/Title.js
+++ src/Title.js
@@ -1,4 +1,4 @@
-import styles from './Title.module.css';
+// import styles from './Title.module.css';
 import { NavLink } from 'react-router-dom'
 import { useState, useEffect, useRef } from 'react'
 
@@ -21,7 +21,7 @@ function Title() {
   return (
     <header
       className='
-        box-border w-full px-8 mb-[2em]
+        box-border w-full px-4 mb-[2em]
         flex items-center gap-8
         bg-brand text-white'
       ref={navRef}
@@ -30,20 +30,41 @@ function Title() {
 
       <nav
         className={`
-          absolute right-4 top-16 z-[9999]
-          w-[100px] bg-white
+          absolute right-6 top-12 z-[9999]
+          w-[100px] border border-brand 
+          bg-white text-brand
           overflow-hidden flex flex-col
-          transition-all duration-300
-          ${isOpen ? 'max-h-[300px]' : ''}`}
+          ${isOpen ? 'max-h-[300px]' : 'max-h-0 border-none'}
+          lg:static lg:w-auto lg:py-1
+          lg:max-h-none
+          lg:flex lg:flex-row 
+          lg:bg-brand lg:text-white border:none
+          `}
         aria-hidden={!isOpen}
       >
         <NavLink
-          className={({ isActive }) => isActive ? styles["nav-link-active"] : styles["nav-link"]}
+          className={({ isActive }) => `
+            px-4 py-4 text-center text-lg
+            lg:py-1
+            hover:bg-brand hover:text-white
+            ${isActive
+              ? 'italic font-bold lg:border-b-2 lg:text-white lg:font-normal'
+              : ''
+            }`
+          }
           onClick={() => setIsOpen(false)}
           to="/">
           List</NavLink>
         <NavLink
-          className={({ isActive }) => isActive ? styles["nav-link-active"] : styles["nav-link"]}
+          className={({ isActive }) => `
+            px-4 py-4 text-center text-lg
+            lg:py-1
+            hover:bg-brand hover:text-white
+            ${isActive
+              ? 'italic font-bold lg:border-b-2 lg:text-white lg:font-normal'
+              : ''
+            }`
+          }
           onClick={() => setIsOpen(false)}
           to="/about">
           About</NavLink>
